57a17b3da341d5564a96bf63b66e76b40c78bee2
Anran Jiao <38322669+jiaoanran@users.noreply.github.com>
2018-10-01 21:45:30 +0800

    单轮多次查询

--- chatbot2.py
+++ chatbot2.py
@@ -3,6 +3,13 @@
 from wxpy import *
 bot = Bot()
 
+from rasa_nlu.training_data import load_data
+from rasa_nlu.config import RasaNLUModelConfig
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+trainer = Trainer(config.load("/Users/Annora/Desktop/chatbot3/config_spacy.yml"))
+training_data = load_data('/Users/Annora/rasa_nlu_chi/data/examples/rasa/rasa-stock.md')
+interpreter = trainer.train(training_data)
 #单轮多次查询
 from iexfinance import Stock
 params = {}
